c9d1ccc3909ef09dc6bea75558ed55ee8df3113b
Bantyn <patelbanty1260@gmail.com>
2025-12-10 21:14:23 +0530

    Done

--- routes/GeminiAiRouter.js
+++ routes/GeminiAiRouter.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-// Import the chatBot controller
 const { chatBot } = require("../controllers/genAiController.js");
 
-// POST /api/genai/genai
 router.post("/", chatBot);
 
 module.exports = router;
